71ff9bfb51dd05dbac1d5c7cb81c50a3325233e9
dongwon.kwak <dongwon.kwak@gmail.com>
2025-06-21 00:31:03 +0900

    백엔드 프로젝트 초기 설정 및 파일 추가: .gitignore, .gitattributes, gradlew, gradlew.bat, build.gradle, settings.gradle, libs.versions.toml, gradle-wrapper 관련 파일 및 BoardlyApplication.java 추가

---
+++ backend/src/main/java/com/boardly/BoardlyApplication.java
@@ -0,0 +1,13 @@
+package com.boardly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardlyApplication.class, args);
+    }
+
+}

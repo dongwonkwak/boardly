d86a489452ff9b027cd871523a9186de471f19e9
dongwon.kwak <dongwon.kwak@gmail.com>
2025-07-05 20:38:00 +0900

    스프링 시큐리티 설정 개선: CORS 및 OAuth2 리소스 서버 설정 추가, 비밀번호 인코더 빈 생성, API 문서화 경로 설정 추가

--- backend/src/main/java/com/boardly/shared/application/config/SecurityConfig.java
+++ backend/src/main/java/com/boardly/shared/application/config/SecurityConfig.java
@@ -1,38 +1,61 @@
 package com.boardly.shared.application.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import com.boardly.shared.presentation.Path;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CorsConfigurationSource corsConfigurationSource;
 
     private static final RequestMatcher[] PUBLIC_MATCHERS = {
             PathPatternRequestMatcher.withDefaults().matcher("/login"),
             PathPatternRequestMatcher.withDefaults().matcher(HttpMethod.GET, "/.well-known/appspecific/com.chrome.devtools.json"),
             // register user
             PathPatternRequestMatcher.withDefaults().matcher(HttpMethod.POST, Path.USERS + "/register"),
+            PathPatternRequestMatcher.withDefaults().matcher("/api-docs/**"),
+            PathPatternRequestMatcher.withDefaults().matcher("/swagger-ui/**"),
+            PathPatternRequestMatcher.withDefaults().matcher("/swagger-ui.html")
     };
 
     @Bean
     @Order(2)
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers(PUBLIC_MATCHERS))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(PUBLIC_MATCHERS).permitAll()
-                        .anyRequest().authenticated());
+            .csrf(csrf -> csrf
+                    .ignoringRequestMatchers(PUBLIC_MATCHERS))
+            .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers(PUBLIC_MATCHERS).permitAll()
+                    .anyRequest().authenticated())
+            .cors(cors -> cors
+                    .configurationSource(corsConfigurationSource))
+            .oauth2ResourceServer(oauth2 -> oauth2
+                    .jwt(Customizer.withDefaults()))
+            ;
 
         return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
